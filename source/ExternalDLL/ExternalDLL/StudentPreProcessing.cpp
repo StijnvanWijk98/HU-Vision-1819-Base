f8436815b3b32e501b87cdbc3ffe64c37ba281cb
Stijn <stijnvanwijk1998@outlook.com>
2020-03-20 15:17:58 +0100

    Updated Implementatieplan & started writing own functions

--- source/ExternalDLL/ExternalDLL/StudentPreProcessing.cpp
+++ source/ExternalDLL/ExternalDLL/StudentPreProcessing.cpp
@@ -1,6 +1,21 @@
 #include "StudentPreProcessing.h"
 
 
+cv::Mat StudentPreProcessing::intensImgToMat(const IntensityImage& image) {
+	cv::Mat res_matrix;
+
+	return res_matrix;
+}
+IntensityImage* StudentPreProcessing::matToIntensImg(const cv::Mat& image) {
+	//IntensityImage res_image;
+
+	return nullptr;
+}
+
+cv::Mat StudentPreProcessing::combineKernels(const cv::Mat& x_res, const cv::Mat& y_res) {
+
+}
+
 IntensityImage * StudentPreProcessing::stepToIntensityImage(const RGBImage &image) const {
 	return nullptr;
 }
@@ -10,6 +25,13 @@ IntensityImage * StudentPreProcessing::stepScaleImage(const IntensityImage &imag
 }
 
 IntensityImage * StudentPreProcessing::stepEdgeDetection(const IntensityImage &image) const {
+	cv::Mat matrix_img;
+	cv::Mat res_kernel_x;
+	cv::Mat res_kernel_y;
+	cv::Mat kernel_x = cv::Mat_<float>(3, 3);
+	cv::Mat kernel_y = cv::Mat_<float>(3, 3);
+	
+	
 	return nullptr;
 }
 
--- source/ExternalDLL/ExternalDLL/StudentPreProcessing.h
+++ source/ExternalDLL/ExternalDLL/StudentPreProcessing.h
@@ -6,10 +6,16 @@
 
 #pragma once
 #include "PreProcessing.h"
+#include <opencv2/core/mat.hpp>
+#include "HereBeDragons.h"
 class StudentPreProcessing : public PreProcessing {
 public:
 	IntensityImage * stepToIntensityImage(const RGBImage &image) const;
 	IntensityImage * stepScaleImage(const IntensityImage &image) const;
 	IntensityImage * stepEdgeDetection(const IntensityImage &image) const;
 	IntensityImage * stepThresholding(const IntensityImage &image) const;
+private:
+	cv::Mat intensImgToMat(const IntensityImage& image);
+	IntensityImage* matToIntensImg(const cv::Mat& image);
+	cv::Mat combineKernels(const cv::Mat& x_res, const cv::Mat& y_res);
 };
